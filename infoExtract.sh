f580bd12550dfcbed0b2035c5567cb45cc87db66
Barnaby Colby <barneycolby@gmail.com>
2014-12-31 12:27:27 -0500

    Fixed to get name of xmlstarlet program from config instead of being hardcoded.

--- infoExtract.sh
+++ infoExtract.sh
@@ -6,6 +6,9 @@ if [[ $# -ne 2 ]]; then
 	exit 1
 fi
 
+configFile='./podcastFeed.cfg'
+source $configFile
+
 # Assign the arguments to variables with more descriptive names
 inputFile=$1
 attributeToExtract=$2
@@ -15,7 +18,7 @@ xpathExpression="/_:program_meta_data/_:$attributeToExtract"
 
 # Use xml starlet to get the attribute value by applying the xpath expression
 # We pipe stderr to null to avoid it being printed to the screen
-attributeValue="$(xml sel --template --value-of $xpathExpression $inputFile 2> /dev/null)"
+attributeValue="$($xmlstarlet sel --template --value-of $xpathExpression $inputFile 2> /dev/null)"
 
 # Store the exit status of the call to xmlstarlet so that we can pass it on
 xmlstarletExitStatus="$?"
